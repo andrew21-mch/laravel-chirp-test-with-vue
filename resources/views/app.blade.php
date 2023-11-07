46140a73a36981a216713a827e286501f5912429
andrew21-mch <nfonandrew73@gmail.com>
2023-11-07 17:17:01 +0100

    update, tetsing crisp for deployment'

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,17 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>\
-    <script type="text/javascript">
-        window.$crisp = [];
-        window.CRISP_WEBSITE_ID = "959fdc24-a0ff-4aa2-90f8-0d10042e89bd";
-        (function() {
-            d = document;
-            s = d.createElement("script");
-            s.src = "https://client.crisp.chat/l.js";
-            s.async = 1;
-            d.getElementsByTagName("head")[0].appendChild(s);
-        })();
-    </script>
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
@@ -30,5 +20,15 @@
 <body class="font-sans antialiased">
     @inertia
 </body>
-
+<script type="text/javascript">
+    window.$crisp = [];
+    window.CRISP_WEBSITE_ID = "959fdc24-a0ff-4aa2-90f8-0d10042e89bd";
+    (function() {
+        d = document;
+        s = d.createElement("script");
+        s.src = "https://client.crisp.chat/l.js";
+        s.async = 1;
+        d.getElementsByTagName("head")[0].appendChild(s);
+    })();
+</script>
 </html>
