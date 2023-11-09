d3e3a1edb4c38af7985aaa4a176fe68a064515a1
andrew21-mch <nfonandrew73@gmail.com>
2023-11-09 06:57:48 +0100

    update, tetsing crisp for deployment'

--- app/Http/Controllers/CrispController.php
+++ app/Http/Controllers/CrispController.php
@@ -16,16 +16,6 @@ class CrispController extends Controller
     {
         $payload = $request->all();
 
-        
-        // Verify the authenticity of the webhook (optional but recommended)
-        // You can implement your own verification logic here
-        // $readable = json_encode($payload);
-        // SendToTelegram::switchnFixNow("crisp submission: $readable");
-        // if user email is leonelngande@gmail, proceed
-        /*if ($payload['data']['user']['email'] === 'leonelngande@gmail.com') {
-            resolve(CrispService::class)->handleWebhookEvents($payload);
-        }*/
-        // resolve(CrispService::class)->logMessage();
         resolve(CrispService::class)->handleWebhookEvents( $payload );
         return response()->json(['success' => true]);
     }
