352b03b996061f6d2bc835fc9ca55e6895e1a129
andrew21-mch <nfonandrew73@gmail.com>
2023-11-09 16:24:06 +0100

    log operators

--- app/Services/CrispService.php
+++ app/Services/CrispService.php
@@ -201,7 +201,7 @@ class CrispService
         // Fetch the list of team members using the Crisp API
         $teamMembers = $this->crispClient->websiteOperators->getList($websiteId);
 
-        $this->logger->info($teamMembers[0]['name']);
+        $this->logger->info("I am here, lets check it out!");
         // Check each team member's availability
         foreach ($teamMembers as $teamMember) {
             $operatorId = $teamMember['id'];
