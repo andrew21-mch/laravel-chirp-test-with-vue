040de327d324fc2be88d336de51edb9825b1be48
andrew21-mch <nfonandrew73@gmail.com>
2023-11-20 09:49:50 +0100

    route protection

--- app/Services/CrispService.php
+++ app/Services/CrispService.php
@@ -270,32 +270,28 @@ class CrispService
 
     public function handleBugReport(string $sessionId, string $websiteId): void
     {
-        $this->sendMessage("You have selected 'Report a bug'. Please provide details about the bug. You can type 'exit' to exit the bug reporting process.", $sessionId, $websiteId);
-
-        // Initialize bug report details
-        $bugDetails = [];
-
-        // Start a loop to collect bug details
         while (true) {
             // Get user input for bug details
             $userInput = $this->getUserInput($sessionId, $websiteId);
-
+        
             // Check if the user wants to exit the bug reporting process
             if (strtolower($userInput) === self::EXIT_COMMAND) {
                 $this->sendMessage("Bug report process has been exited.", $sessionId, $websiteId);
                 return;
             }
-
+        
             // Add the user input to bug details
             $bugDetails[] = $userInput;
-
+        
             // Ask the user for additional details or confirm submission
             $confirmationMessage = "Bug details collected so far:\n" . implode("\n", $bugDetails) . "\n\nType 'exit' to submit the bug report or provide additional details:";
             $this->sendMessage($confirmationMessage, $sessionId, $websiteId);
-
+        
+            sleep(1); // Adjust the sleep duration as needed
+        
             // Wait for user response
             $confirmation = $this->getUserInput($sessionId, $websiteId);
-
+        
             // Check if the user wants to exit or submit the bug report
             if (strtolower($confirmation) === self::EXIT_COMMAND) {
                 $this->sendMessage("Bug report process has been exited.", $sessionId, $websiteId);
@@ -308,6 +304,7 @@ class CrispService
                 $this->sendMessage("Bug details updated. Type 'exit' to submit the bug report or provide additional details:", $sessionId, $websiteId);
             }
         }
+        
     }
     private function reportAirtimeNotReceived(string $sessionId, string $websiteId): void
     {
