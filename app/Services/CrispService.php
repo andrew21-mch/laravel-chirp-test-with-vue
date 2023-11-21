01662a94f22ecb13a1c5163b4670bd57ce222966
andrew21-mch <nfonandrew73@gmail.com>
2023-11-21 10:18:16 +0100

    set back to prev

--- app/Services/CrispService.php
+++ app/Services/CrispService.php
@@ -174,8 +174,7 @@ class CrispService
                         $this->handleInquiries($sessionId, $websiteId);
                         break;
                     case 'airtime_not_received':
-                        $transactionReporter = new TransactionReporter($this, $this->logger);
-                        $transactionReporter->reportAirtimeNotReceived($sessionId, $websiteId);
+                        $this->reportAirtimeNotReceived($sessionId, $websiteId);
                         break;
 
                     case 'bug_report':
@@ -223,6 +222,73 @@ class CrispService
         $this->sendMessage($menuMessage, $sessionId, $websiteId);
     }
 
+    public function reportAirtimeNotReceived(string $sessionId, string $websiteId): void
+    {
+        try {
+            $userInput = $this->getUserInput($sessionId, $websiteId, "You have selected 'Report airtime not received'. Please send us your transaction message received from switchn, mtn, orange, or camtel.");
+
+            $transactionDetails = $this->scanMessage($userInput);
+
+            if (!empty($transactionDetails)) {
+                $this->handleSuccessfulTransaction($transactionDetails, $sessionId, $websiteId);
+            } else {
+                $this->handleFailedTransaction($sessionId, $websiteId);
+            }
+        } catch (\Exception $e) {
+            // Log or handle the exception as needed
+            $this->logger->error("Error during 'reportAirtimeNotReceived' handling: " . $e->getMessage());
+        }
+    }
+
+
+    private function handleSuccessfulTransaction(array $transactionDetails, string $sessionId, string $websiteId): void
+    {
+        // Handle a successful transaction
+        $sender = $transactionDetails['external_transaction_id'];
+        $amount = $transactionDetails['amount'];
+        $date = $transactionDetails['date'];
+        $transactionId = $transactionDetails['transaction_id'];
+
+        $response = "Transaction details received:
+                    \n## Extrernal ID: $sender
+                    \n## Transaction ID : $transactionId
+                    \n## Amount: $amount
+                    \n## Date: $date";
+        $this->sendMessage($response, $sessionId, $websiteId);
+    }
+
+    private function handleFailedTransaction(string $sessionId, string $websiteId): void
+    {
+        // Handle a failed transaction
+        $this->sendMessage("Unable to extract transaction details. Please provide a valid transaction message.", $sessionId, $websiteId);
+    }
+    private function scanMessage(string $message): array
+    {
+        $pattern = '/Hello, the transaction with amount (?P<amount>\d+ \w+) for (?P<payment_method>[\w\s]+) \((?P<payment_id>\d+)\) with message: (?P<message>.+?) at (?P<date>\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}), reason: (?P<reason>[\w\s.]+) .Please check your balance or contact system admin for more information. Financial Transaction Id: (?P<transaction_id>\d+). External Transaction Id: (?P<external_transaction_id>\d+)./';
+
+        preg_match($pattern, $message, $matches);
+
+        // Provide default values in case of no match
+        $defaultValues = [
+            'amount' => 'N/A',
+            'payment_method' => 'N/A',
+            'payment_id' => 'N/A',
+            'message' => 'N/A',
+            'date' => 'N/A',
+            'reason' => 'N/A',
+            'transaction_id' => 'N/A',
+            'external_transaction_id' => 'N/A',
+        ];
+
+        // Combine default values with matched values
+        $transactionInfo = array_merge($defaultValues, $matches);
+
+        // Remove the full match from the result
+        unset($transactionInfo[0]);
+
+        return $transactionInfo;
+    }
+
 
     private function handleSelectedOption(string $selectedOption, string $sessionId, string $websiteId, string $userId = null): void
     {
@@ -234,7 +300,7 @@ class CrispService
                 $this->handleBugReport($sessionId, $websiteId);
                 break;
             case '2':
-                $transactionReporter = new TransactionReporter();
+                $transactionReporter = new TransactionReporter($this, $this->logger);
                 $transactionReporter->reportAirtimeNotReceived($sessionId, $websiteId);
                 break;
             case '3':
@@ -515,8 +581,4 @@ class CrispService
         $transactionResponse = "Thank you for your transaction message! We are processing your request.";
         $this->sendMessage($transactionResponse, $sessionId, $websiteId);
     }
-
-
-
-
 }
